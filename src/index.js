e52e40182e5d1763b9a82099edc4d2286dd0295b
Amancio <amancio.jluis@outlook.com>
2025-07-10 09:00:17 -0300

    JLA - Estruturação do projeto, install: chalk, qrcode-terminal, refatoração index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import prompt from "prompt";
 import mainPrompt from "./prompts/prompt.main.js";
+import createQrCode from "./qrcode/createQrCode.js";
+
 
 
 async function main() {
         prompt.get(mainPrompt, async (err, choice) =>{
-        if(choice.select == 1){
-            console.log('Escolheu a opção Geraçao de QRCode');
-        }
-        if(choice.select == 2){
-            console.log('Escolheu a opção Geração de assword');
-        }
+
+        if(choice.select == 1) await createQrCode();
+        if(choice.select == 2) console.log('Escolheu a opção Geração de password');
+        
         prompt.start();
     })
 }
---
+++ src/services/qrcode/createQrCode.js
@@ -0,0 +1,11 @@
+import prompt from "prompt";
+import promptQrCode from "../../prompts/prompt-qrcode.js";
+
+
+import mainPrompt from "./prompts/prompt.main.js";
+
+async function createQrCode(){
+
+}
+
+export default createQrCode;
